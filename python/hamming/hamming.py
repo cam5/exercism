0f338f6a9fe6499dae5ead56b82937976beac7ff
Cameron Hurd <me@cameronhurd.com>
2018-05-27 22:33:46 -0400

    adds and solves "hamming" in python

---
+++ python/hamming/hamming.py
@@ -0,0 +1,11 @@
+def distance(strand_a, strand_b):
+    if (len(strand_a) != len(strand_b)):
+        raise ValueError('Different strand lengths')
+
+    distance = 0
+
+    for i in range(0, len(strand_a)):
+        if (strand_a[i] != strand_b[i]):
+            distance += 1
+
+    return distance
