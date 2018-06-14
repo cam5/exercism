8b8075ee7357887e164f1ab7a973a9b5d3286601
Cameron Hurd <me@cameronhurd.com>
2018-06-14 18:24:19 -0400

    get bob passing

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,9 +1,25 @@
 import re
 
 def hey(phrase):
+    phrase   = phrase.strip()
     question = re.compile(r'.*\?$')
+    letters  = ''.join([l for l in phrase if re.match(r'\w', l)])
+    yelling  = False
+
+    if (True == letters.isupper()):
+        yelling = True
+
+    if (True == yelling):
+        if (question.match(phrase)):
+            return "Calm down, I know what I'm doing!"
+        else:
+            return 'Whoa, chill out!'
 
     if (question.match(phrase)):
         return 'Sure.'
+
+    if (letters.strip() == ''):
+        return 'Fine. Be that way!'
+
 
     return 'Whatever.'
