488f5fc3a86adfcc67bdda63e60c7f127ca8b3a1
Cameron Hurd <me@cameronhurd.com>
2018-07-31 15:48:02 -0400

    adds a little note that helps pass flake8 linter :)

--- python/hello-world/hello_world.py
+++ python/hello-world/hello_world.py
@@ -1,2 +1,8 @@
+"""This is a module for use with the hello_world exercise on the python track
+of exercism."""
+
+
 def hello(name=''):
+    """Given a a name, greets specifically; otherwise, generically."""
+
     return 'Hello, World!' if name == "" else "Hello, " + name + "!"
