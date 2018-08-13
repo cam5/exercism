5b118b0bd11f2caeb7d74a619bb5a25586b09928
Cameron Hurd <me@cameronhurd.com>
2018-08-13 17:56:28 -0400

    solve bracket push

--- python/bracket-push/bracket_push.py
+++ python/bracket-push/bracket_push.py
@@ -9,9 +9,7 @@ def is_paired(input_string):
     """
     tests if a string has matching brackets.
 
-    The supplied test does not check for wether or not the brackets are
-    inverse.
-    i.e. ][ - will technically pass this test for the moment.
+    (will not catch quoted parens.)
     """
 
     input_array = list(input_string)
@@ -30,6 +28,24 @@ def is_paired(input_string):
         if opening_counts[char] != closing_counts[CHAR_MAP[char]]:
             balanced = False
 
-    print(opening_counts, closing_counts)
+    well_ordered = True
 
-    return balanced
+    if input_string != '' and input_string[0] in CLOSING_CHARS:
+        well_ordered = False
+
+    opened_parens = []
+
+    for char in input_string:
+        if char in OPENING_CHARS:
+            opened_parens.append(char)
+
+        # If We've logged opening parens, and now we're at a closing one...
+        if char in CLOSING_CHARS and opened_parens:
+            # Make sure that it matches the most-recently opened paren.
+            if char != CHAR_MAP.get(opened_parens[-1], False):
+                well_ordered = False
+
+            # Move cursor to next most recent
+            opened_parens.pop()
+
+    return balanced and well_ordered
