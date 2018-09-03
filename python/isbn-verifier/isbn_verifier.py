81d5410b88732747cb1bfcb1392bc1a351ea1901
Cameron Hurd <me@cameronhurd.com>
2018-09-03 16:21:29 -0400

    lints the isbn exercise

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,26 +1,30 @@
+"""Isbn Verifier Exercise from exercism.io"""
+
+
 def verify(isbn):
+    """Given an input string, validate wether or not it is an ISBN"""
     char_list = [d for d in list(isbn) if d != '-']
 
     # don't allow lists of less than 10 digits
-    if (len(char_list) < 10):
+    if len(char_list) < 10:
         return False
 
     # Replace our check digit if it's an X
-    if (char_list[-1] == 'X'):
+    if char_list[-1] == 'X':
         char_list[-1] = 10
 
     # If there's something other than a number at this point, it's invalid.
     for char in char_list:
-        if isinstance(char, str) and False == char.isnumeric():
+        if isinstance(char, str) and char.isnumeric() is False:
             return False
 
     factor = 10
-    val    = 0
+    val = 0
 
     digit_list = [int(d) for d in char_list]
 
     for digit in digit_list:
-        val    = val + digit * factor
+        val = val + digit * factor
         factor = factor - 1
 
-    return 0 == (val % 11)
+    return val % 11 == 0
