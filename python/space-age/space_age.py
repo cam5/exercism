aed5180c9e84c05d9bffb12b66d0c292a6fc21c1
Cameron Hurd <me@cameronhurd.com>
2018-11-01 17:36:38 -0400

    solves space_age

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -4,10 +4,14 @@ Exercism.ui problem set = spacage!
 https://exercism.io/my/solutions/7e08cf5c963843f083dfca3a04d54acc
 """
 
-DAYS_IN_EARTH_YEAR = 365.25
-SECONDS_IN_EARTH_DAY = 86400  # 60 * 60 * 24
+"""
+Orbital periods, relative to earth.
 
+Ex. In the time it takes Mercury to make one full revolution of the sun, Earth
+will only have orbited 0.2408467 of the way.
+"""
 ORBITAL_PERIODS = {
+    'EARTH': 1,
     'MERCURY': 0.2408467,
     'VENUS': 0.61519726,
     'MARS': 1.8808158,
@@ -18,21 +22,32 @@ ORBITAL_PERIODS = {
 }
 
 
-# pylint: disable=too-few-public-methods
 class SpaceAge(object):
     """
-    Main class for the problem. :)
+    Handles data and calculations concerning the age of a person on different
+    planets, given a number of 'earth seconds'.
     """
     def __init__(self, seconds):
         self.seconds = seconds
 
     def earth_years(self):
-        return (self.seconds / SECONDS_IN_EARTH_DAY) * DAYS_IN_EARTH_YEAR
+        """
+        Calculate the number of earth revolutions that a person would have been
+        present for, given a number of seconds.
+        """
+        days_in_earth_year = 365.25
+        seconds_in_earth_day = 86400  # 60 * 60 * 24
+
+        return self.seconds / (seconds_in_earth_day * days_in_earth_year)
 
     def __getattr__(self, attrname):
+        """
+        Returns a function calculating age in {planet} years, where the planet
+        is known, else, a regular getattr call.
+        """
         planet = attrname[3:].upper()
         if planet in ORBITAL_PERIODS:
             factor = ORBITAL_PERIODS.get(planet)
-            return lambda: self.earth_years() * factor
+            return lambda: round(self.earth_years() / factor, 2)
 
-        raise NameError
+        return getattr(self, attrname)
