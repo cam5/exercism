31ca462b9cfeb7f769e80d2c51aeca085371cedb
Cameron Hurd <me@cameronhurd.com>
2022-05-24 22:31:36 -0400

    improve lettering or whatever

--- lua/isogram/isogram.lua
+++ lua/isogram/isogram.lua
@@ -1,9 +1,17 @@
+function printtable(table, indent)
+  print(tostring(table) .. '\n')
+  for index, value in pairs(table) do
+    print('    ' .. tostring(index) .. ' : ' .. tostring(value) .. '\n')
+  end
+end
+
 return function(s)
   t = {}
   isogram = true
+  s = s:lower()
 
-  for str in string.gmatch(s:lower(), ".") do
-    if (t[str] ~= nil) then
+  for str in string.gmatch(s, ".") do
+    if (table.concat(t):find(str)) then
       isogram = false
     end
 
